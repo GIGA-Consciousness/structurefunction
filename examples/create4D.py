a0d0918765f1cbd983e76f6f6534cb4f07901841
Erik <erik.ziegler@ulg.ac.be>
2014-03-13 14:07:57 +0100

    Script for created 4D nifti files from messy directories

---
+++ examples/create4D.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+import sys
+
+def create4Ddwi(name,out):
+	import glob
+	print("Looking for %s" % name)
+	name = str(name)
+	out = str(out)
+	a = glob.glob(name + "*.img")
+	assert(len(a) == 130)
+	a.sort(key=lambda x: x[-8:])
+	import nibabel as nb
+	print("Concatenating %i files" % len(a))
+	b = nb.concat_images(a)
+	b.get_header()
+	out_file = out+".nii.gz"
+	print("Writing to %s" % out_file)
+	nb.save(b, out_file)
+	return out_file
+
+if __name__ == '__main__':
+	name, out = sys.argv[1:]
+	create4Ddwi(name, out)
