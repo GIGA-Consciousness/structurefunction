3e37d82e46539b28e29f23a0624d334057338e95
Erik Ziegler <erik.ziegler@ulg.ac.be>
2014-03-13 14:28:11 +0100

    Split PreCoTh into THREE steps. First is motion and eddy correction, followed by manual vibration correction. Second is tensor stuff, Third is tracking

---
+++ examples/example_fixvibration.py
@@ -0,0 +1,9 @@
+from coma.interfaces.dti import remove_bad_volumes, nonlinfit_fn
+
+in_dwi = "Bend1_DTI64dirs029a001_motion_eddy.nii.gz"
+in_bvec = "Bend1_DTI64dirs029a001_rotated.bvec"
+in_bval = "Bend1_DTI64dirs029a001.bval"
+x_thresh = 0.55
+
+corr_dwi, corr_bvec, corr_bval, n = remove_bad_volumes(in_dwi, in_bvec, in_bval, x_thresh)
+nonlinfit_fn(corr_dwi, corr_bvec, corr_bval, "Corrected")
