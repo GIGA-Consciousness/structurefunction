35e11d23a7a537183c623d948b7ac300743988c6
Erik Ziegler <erik.ziegler@ulg.ac.be>
2014-04-21 11:34:46 +0200

    In progress stuff for easy track bundling using QuickBundles

--- examples/example_quickbundle.py
+++ examples/example_quickbundle.py
@@ -5,4 +5,5 @@ in_file = op.abspath("/Code/structurefunction/examples/"\
     "precoth/ex_precoth/coma_precoth/_subject_id_Bend1/"\
     "thalamus2precuneus2cortex/converted_intersections_streamline_final.trk")
 
+in_file = op.abspath("ExampleTracks.trk")
 out_files = bundle_tracks(in_file)
