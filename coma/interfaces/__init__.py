761b403858f2340b1edb1ec60796084073b7bea5
Erik Ziegler <erik.ziegler@ulg.ac.be>
2014-05-19 19:51:10 +0200

    Almost-working wrapper for PVELab

--- coma/interfaces/__init__.py
+++ coma/interfaces/__init__.py
@@ -1,6 +1,7 @@
 from .base import CreateDenoisedImage, MatchingClassification, ComputeFingerprint
 from .dti import nonlinfit_fn
-from .graphs import CreateConnectivityThreshold, ConnectivityGraph
 from .functional import RegionalValues, SimpleTimeCourseCorrelationGraph
 from .gift import SingleSubjectICA
+from .graphs import CreateConnectivityThreshold, ConnectivityGraph
 from .glucose import CMR_glucose
+from .pve import PartialVolumeCorrection
