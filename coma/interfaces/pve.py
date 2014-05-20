b3093c3aa9d01f9caeea3fe22909db2d8c36824d
Erik Ziegler <erik.ziegler@ulg.ac.be>
2014-05-21 00:05:05 +0200

    PVE pipeline works, but affines in files converted from Analyze to Nifti don't seem to match original files. r_volume_GMROI must be uint8

--- coma/interfaces/pve.py
+++ coma/interfaces/pve.py
@@ -11,65 +11,128 @@ import glob
 import logging
 import numpy as np
 import random
-
+import gzip
+import shutil
 logging.basicConfig()
 iflogger = logging.getLogger('interface')
 
+
 def nifti_to_analyze(nii):
     nifti = nb.load(nii)
-    nif = open(nii, 'rb')
+    if nii[-3:] == '.gz':
+        nif = gzip.open(nii, 'rb')
+    else:
+        nif = open(nii, 'rb')
     hdr = nb.nifti1.Nifti1Header.from_fileobj(nif)
+
     arr_hdr = nb.analyze.AnalyzeHeader.from_header(hdr)
     arrb = hdr.raw_data_from_fileobj(nif)
-    img = nb.AnalyzeImage(dataobj=arrb, affine=nifti.get_affine(), header=arr_hdr)
+    img = nb.AnalyzeImage(
+        dataobj=arrb, affine=nifti.get_affine(), header=arr_hdr)
     _, name, _ = split_filename(nii)
     nb.analyze.save(img, op.abspath(name + '.img'))
     return op.abspath(name + '.img'), op.abspath(name + '.hdr')
 
+
 def analyze_to_nifti(img, ext='.nii.gz'):
-    img = nb.load(img)
-    nii = nb.Nifti1Image.from_image(img)
+    image = nb.load(img)
+    nii = nb.Nifti1Image.from_image(image)
     _, name, _ = split_filename(img)
+    affine = image.get_affine()
+    nii.set_sform(affine)
+    nii.set_qform(affine)
     nb.save(nii, op.abspath(name + ext))
     return op.abspath(name + ext)
+
+
+def fix_roi_values(roi_image, white_matter_file, csf_file):
+    '''
+    Changes ROI values to prevent values equal to 1, 2,
+    or 3. These are reserved for GM/WM/CSF in the PVELab
+    functions.
+    '''
+
+    image = nb.load(roi_image)
+    data = image.get_data()
+    data = data.astype(np.uint8)
+    data[data > 0] = data[data > 0] + 50
+
+    wm_image = nb.load(white_matter_file)
+    wm_data = wm_image.get_data()
+    csf_image = nb.load(csf_file)
+    csf_data = csf_image.get_data()
+
+    assert (data.shape == wm_data.shape == csf_data.shape)
+    data[(wm_data > 0)*(data == 0)] = 2
+    data[(csf_data > 0)*(data == 0)] = 3
+
+    hdr = image.get_header()
+    fixed = nb.Nifti1Image(
+        dataobj=data, affine=image.get_affine(), header=hdr)
+    _, name, _ = split_filename(roi_image)
+    fixed.set_data_dtype(np.uint8)
+    fixed_roi_image = op.abspath(name + "_p100.nii.gz")
+    nb.save(fixed, fixed_roi_image)
+    return fixed_roi_image
+
+def switch_datatype(in_file, dt=np.uint8):
+    '''
+    Changes ROI values to prevent values equal to 1, 2,
+    or 3. These are reserved for GM/WM/CSF in the PVELab
+    functions.
+    '''
+
+    image = nb.load(in_file)
+    image.set_data_dtype(dt)
+    _, name, _ = split_filename(in_file)
+    fixed_image = op.abspath(name + "_u8.nii.gz")
+    nb.save(image, fixed_image)
+    return fixed_image
 
 def write_config_dat(roi_file, use_fs_LUT=False):
     out_file = "ROI_names.dat"
     f = open(out_file, "w")
-    f.write("Report ROI coding #, ROI Name and color in hexaddecimal, separated by a <Tab> hereinafter, with no space at the end\n")
+    f.write(
+        "Report ROI coding #, ROI Name and color in hexaddecimal, separated by a <Tab> hereinafter, with no space at the end\n")
     image = nb.load(roi_file)
     data = image.get_data()
     IDs = np.unique(data)
     IDs.sort()
     IDs = IDs.tolist()
-    if 0 in IDs: IDs.remove(0)
-    if 1 in IDs: IDs.remove(1)
-    if 2 in IDs: IDs.remove(2)
-    if 3 in IDs: IDs.remove(3)
-    r = lambda: random.randint(0,255)
+    if 0 in IDs:
+        IDs.remove(0)
+    if 1 in IDs:
+        IDs.remove(1)
+    if 2 in IDs:
+        IDs.remove(2)
+    if 3 in IDs:
+        IDs.remove(3)
+    r = lambda: random.randint(0, 255)
     for idx, val in enumerate(IDs):
         # e.g. 81  R_Hippocampus       008000
-        f.write("%i\tRegion%i\t%02X%02X%02X\n" % (val, idx, r(),r(),r()))
+        f.write("%i\tRegion%i\t%02X%02X%02X\n" % (val, idx, r(), r(), r()))
     f.close()
     return op.abspath(out_file)
 
+
 class PartialVolumeCorrectionInputSpec(BaseInterfaceInputSpec):
     pet_file = File(exists=True, mandatory=True,
-                              desc='The input PET image')
+                    desc='The input PET image')
     t1_file = File(exists=True, mandatory=True,
-        desc='The input T1')
+                   desc='The input T1')
     white_matter_file = File(exists=True,
-        desc='Segmented white matter')
+                             desc='Segmented white matter')
     grey_matter_file = File(exists=True,
-        desc='Segmented grey matter')
+                            desc='Segmented grey matter')
     csf_file = File(exists=True,
-        desc='Segmented cerebrospinal fluid')
+                    desc='Segmented cerebrospinal fluid')
     roi_file = File(exists=True, mandatory=True, xor=['skip_atlas'],
-        desc='The input ROI image')
+                    desc='The input ROI image')
     skip_atlas = traits.Bool(xor=['roi_file'],
-        desc='Uses the WM/GM/CSF segmentation instead of an atlas')
+                             desc='Uses the WM/GM/CSF segmentation instead of an atlas')
     use_fs_LUT = traits.Bool(True, usedefault=True,
-        desc='Uses the Freesurfer lookup table for names in the atlas')
+                             desc='Uses the Freesurfer lookup table for names in the atlas')
+
 
 class PartialVolumeCorrectionOutputSpec(TraitedSpec):
     alfano_alfano = File(
@@ -93,13 +156,14 @@ class PartialVolumeCorrectionOutputSpec(TraitedSpec):
     mueller_gartner_WMroi = File(
         exists=True, desc='mueller_gartner_WMroi')
     virtual_pet_image = File(
-        exists=True, desc='virtual_pet_image') 
+        exists=True, desc='virtual_pet_image')
     white_matter_roi = File(
-        exists=True, desc='white_matter_roi') 
+        exists=True, desc='white_matter_roi')
     rousset_mat_file = File(
-        exists=True, desc='rousset_mat_file') 
+        exists=True, desc='rousset_mat_file')
     point_spread_image = File(
-        exists=True, desc='point_spread_image') 
+        exists=True, desc='point_spread_image')
+
 
 class PartialVolumeCorrection(BaseInterface):
 
@@ -122,16 +186,24 @@ class PartialVolumeCorrection(BaseInterface):
         f.write("%s;%s" % (pet_path, t1_path))
         f.close()
 
-        gm_path, _ = nifti_to_analyze(self.inputs.grey_matter_file)
+        gm_uint8 = switch_datatype(self.inputs.grey_matter_file)
+        gm_path, _ = nifti_to_analyze(gm_uint8)
         iflogger.info("Writing to %s" % gm_path)
-        wm_path, _ = nifti_to_analyze(self.inputs.white_matter_file)
+
+        wm_uint8 = switch_datatype(self.inputs.white_matter_file)
+        wm_path, _ = nifti_to_analyze(wm_uint8)
         iflogger.info("Writing to %s" % wm_path)
-        csf_path, _ = nifti_to_analyze(self.inputs.csf_file)
+
+        csf_uint8 = switch_datatype(self.inputs.csf_file)
+        csf_path, _ = nifti_to_analyze(csf_uint8)
         iflogger.info("Writing to %s" % csf_path)
-        rois_path, _ = nifti_to_analyze(self.inputs.roi_file)
+
+        fixed_roi_file = fix_roi_values(self.inputs.roi_file, self.inputs.white_matter_file, self.inputs.csf_file)
+        rois_path, _ = nifti_to_analyze(fixed_roi_file)
         iflogger.info("Writing to %s" % rois_path)
 
-        dat_path = write_config_dat(self.inputs.roi_file, self.inputs.use_fs_LUT)
+        dat_path = write_config_dat(
+            fixed_roi_file, self.inputs.use_fs_LUT)
         iflogger.info("Writing to %s" % dat_path)
 
         d = dict(
@@ -151,52 +223,58 @@ class PartialVolumeCorrection(BaseInterface):
         runbatch_nogui(filelist, gm, wm, csf, rois, dat)
         """).substitute(d)
         mlab = MatlabCommand(script=script, mfile=True,
-                               prescript=[''], postscript=[''])
+                             prescript=[''], postscript=[''])
         result = mlab.run()
 
-
-        occu_MG_img = glob.glob("r_volume_Occu_MG.img")[0]
+        _, foldername, _ = split_filename(self.inputs.pet_file)
+        occu_MG_img = glob.glob("pve_%s/r_volume_Occu_MG.img" % foldername)[0] 
         analyze_to_nifti(occu_MG_img)
-        occu_meltzer_img = glob.glob("r_volume_Occu_Meltzer.img")[0]
+        occu_meltzer_img = glob.glob("pve_%s/r_volume_Occu_Meltzer.img" % foldername)[0] 
         analyze_to_nifti(occu_meltzer_img)
-        meltzer_img = glob.glob("r_volume_Meltzer.img")[0]
+        meltzer_img = glob.glob("pve_%s/r_volume_Meltzer.img" % foldername)[0] 
         analyze_to_nifti(meltzer_img)
-        MG_rousset_img = glob.glob("r_volume_MGRousset.img")[0]
+        MG_rousset_img = glob.glob("pve_%s/r_volume_MGRousset.img" % foldername)[0] 
         analyze_to_nifti(MG_rousset_img)
-        MGCS_img = glob.glob("r_volume_MGCS.img")[0]
+        MGCS_img = glob.glob("pve_%s/r_volume_MGCS.img" % foldername)[0] 
         analyze_to_nifti(MGCS_img)
-        virtual_PET_img = glob.glob("r_volume_Virtual_PET.img")[0]
+        virtual_PET_img = glob.glob("pve_%s/r_volume_Virtual_PET.img" % foldername)[0] 
         analyze_to_nifti(virtual_PET_img)
-        centrum_semiovalue_WM_img = glob.glob("r_volume_CSWMROI.img")[0]
+        centrum_semiovalue_WM_img = glob.glob("pve_%s/r_volume_CSWMROI.img" % foldername)[0] 
         analyze_to_nifti(centrum_semiovalue_WM_img)
-        alfano_alfano_img = glob.glob("r_volume_AlfanoAlfano.img")[0]
+        alfano_alfano_img = glob.glob("pve_%s/r_volume_AlfanoAlfano.img" % foldername)[0] 
         analyze_to_nifti(alfano_alfano_img)
-        alfano_cs_img = glob.glob("r_volume_AlfanoCS.img")[0]
+        alfano_cs_img = glob.glob("pve_%s/r_volume_AlfanoCS.img" % foldername)[0] 
         analyze_to_nifti(alfano_cs_img)
-        alfano_rousset_img = glob.glob("r_volume_AlfanoRousset.img")[0]
+        alfano_rousset_img = glob.glob("pve_%s/r_volume_AlfanoRousset.img" % foldername)[0] 
         analyze_to_nifti(alfano_rousset_img)
-        mg_alfano_img = glob.glob("r_volume_MGAlfano.img")[0]
+        mg_alfano_img = glob.glob("pve_%s/r_volume_MGAlfano.img" % foldername)[0] 
         analyze_to_nifti(mg_alfano_img)
-        mask_img = glob.glob("r_volume_Mask.img")[0]
+        mask_img = glob.glob("pve_%s/r_volume_Mask.img" % foldername)[0] 
         analyze_to_nifti(mask_img)
-        PSF_img = glob.glob("r_volume_PSF.img")[0]
+        PSF_img = glob.glob("pve_%s/r_volume_PSF.img" % foldername)[0] 
         analyze_to_nifti(PSF_img)
+
+        rousset_mat_file = glob.glob("pve_%s/r_volume_Rousset.mat" % foldername)[0]
+        shutil.copyfile(rousset_mat_file, op.abspath("r_volume_Rousset.mat"))
         return result.runtime
 
     def _list_outputs(self):
         outputs = self._outputs().get()
-        outputs['occu_mueller_gartner'] = op.abspath("r_volume_Occu_MG.nii")
-        outputs['occu_meltzer'] = op.abspath("r_volume_Occu_Meltzer.nii")
-        outputs['meltzer'] = op.abspath("r_volume_Meltzer.nii")
-        outputs['mueller_gartner_rousset'] = op.abspath("r_volume_MGRousset.nii")
-        outputs['mueller_gartner_WMroi'] = op.abspath("r_volume_MGCS.nii")
-        outputs['virtual_pet_image'] = op.abspath("r_volume_Virtual_PET.nii")
-        outputs['white_matter_roi'] = op.abspath("r_volume_CSWMROI.nii")
+        outputs['occu_mueller_gartner'] = op.abspath("r_volume_Occu_MG.nii.gz")
+        outputs['occu_meltzer'] = op.abspath("r_volume_Occu_Meltzer.nii.gz")
+        outputs['meltzer'] = op.abspath("r_volume_Meltzer.nii.gz")
+        outputs['mueller_gartner_rousset'] = op.abspath(
+            "r_volume_MGRousset.nii.gz")
+        outputs['mueller_gartner_WMroi'] = op.abspath("r_volume_MGCS.nii.gz")
+        outputs['virtual_pet_image'] = op.abspath(
+            "r_volume_Virtual_PET.nii.gz")
+        outputs['white_matter_roi'] = op.abspath("r_volume_CSWMROI.nii.gz")
         outputs['rousset_mat_file'] = op.abspath("r_volume_Rousset.mat")
-        outputs['point_spread_image'] = op.abspath("r_volume_PSF.nii")
-        outputs['mask'] = op.abspath("r_volume_Mask.nii")
-        outputs['alfano_alfano'] = op.abspath("r_volume_AlfanoAlfano.nii")
-        outputs['alfano_cs'] = op.abspath("r_volume_AlfanoCS.nii")
-        outputs['alfano_rousset'] = op.abspath("r_volume_AlfanoRousset.nii")
-        outputs['mueller_gartner_alfano'] = op.abspath("r_volume_MGAlfano.nii")
+        outputs['point_spread_image'] = op.abspath("r_volume_PSF.nii.gz")
+        outputs['mask'] = op.abspath("r_volume_Mask.nii.gz")
+        outputs['alfano_alfano'] = op.abspath("r_volume_AlfanoAlfano.nii.gz")
+        outputs['alfano_cs'] = op.abspath("r_volume_AlfanoCS.nii.gz")
+        outputs['alfano_rousset'] = op.abspath("r_volume_AlfanoRousset.nii.gz")
+        outputs['mueller_gartner_alfano'] = op.abspath(
+            "r_volume_MGAlfano.nii.gz")
         return outputs
