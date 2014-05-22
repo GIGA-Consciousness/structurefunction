ba4737a79d2beaa132879258ca85333b1e532e0c
Erik Ziegler <erik.ziegler@ulg.ac.be>
2014-05-23 00:29:12 +0200

    Fixed plotting script for FSPet. PET looks weird though

--- examples/example_plot_fspet.py
+++ examples/example_plot_fspet.py
@@ -2,16 +2,22 @@ import os
 import os.path as op
 from surfer import Brain, project_volume_data
 
-subject_id = "fsaverage"
+from coma.datasets import sample
+data_path = sample.data_path()
+
+subjects_dir = op.join(data_path, "subjects")
+os.environ['SUBJECTS_DIR'] = subjects_dir
+
+subject_id = "Bend1"
 hemi = "lh"
 surf = "pial"
 
-brain = Brain(subject_id, hemi, surf)
-volume_file = op.abspath("example_fspet/corrected_pet_to_t1/_subject_id_Bend1/r_volume_MGRousset_flirt.nii")
-volume_file = op.abspath("ComaSample/data/Bend1/petmr.nii.gz")
-pet = project_volume_data(volume_file, hemi,
-                            subject_id=subject_id, smooth_fwhm=0.5)
-#brain.add_overlay(pet, min=0.1, max=15000)
+brain = Brain(subject_id, hemi, surf, subjects_dir=subjects_dir)
+volume_file = op.abspath(
+    "example_fspet/corrected_pet_to_t1/_subject_id_Bend1/r_volume_MGRousset_flirt.nii")
 
-brain.add_data(pet, min=0, max=60000, thresh=.5,
-               colormap="jet", alpha=.6, colorbar=False)
+pet = project_volume_data(volume_file, hemi,
+                          subject_id=subject_id, smooth_fwhm=0.5)
+
+brain.add_data(pet, 
+               colormap="jet", alpha=.6, colorbar=True)
