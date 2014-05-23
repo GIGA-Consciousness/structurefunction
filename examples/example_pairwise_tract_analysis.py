33f42e4649272ff6dfea980474c9b3feac8ffd13
Erik Ziegler <erik.ziegler@ulg.ac.be>
2014-05-23 02:13:09 +0200

    Tract specific analysis runs. Need to get fslstats outputs in python

--- examples/example_pairwise_tract_analysis.py
+++ examples/example_pairwise_tract_analysis.py
@@ -9,10 +9,16 @@ fsl.FSLCommand.set_default_output_type('NIFTI_GZ')
 
 from coma.workflows.dmn import create_paired_tract_analysis_wf
 
+path = op.abspath("/Users/erik/Dropbox/Thesis/Data/Bend1")
+
 paired = create_paired_tract_analysis_wf("example_paired")
-paired
-
-
+paired.base_dir = op.abspath("example_paired")
+paired.inputs.inputnode.fa = op.join(path,"Bend1_fa.nii.gz")
+paired.inputs.inputnode.md = op.join(path,"Bend1_md.nii.gz")
+paired.inputs.inputnode.roi_file = op.join(path,"conform_Bend1_rois_flirt.nii.gz")
+paired.inputs.inputnode.track_file = op.join(path,"Bend1_tracks_50k.tck")
 paired.write_graph()
+paired.config['execution'] = {'remove_unnecessary_outputs': 'false',
+                                   'hash_method': 'timestamp'}
 paired.run()
 #workflow.run(plugin='MultiProc', plugin_args={'n_procs' : 4})
