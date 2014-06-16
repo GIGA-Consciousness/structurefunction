1ababf5b38b3e5cdf5c086330a8dafd4ab03fb61
Erik <erik.ziegler@ulg.ac.be>
2014-06-16 12:43:00 +0200

    update init

--- coma/interfaces/__init__.py
+++ coma/interfaces/__init__.py
@@ -5,3 +5,4 @@ from .gift import SingleSubjectICA
 from .graphs import CreateConnectivityThreshold, ConnectivityGraph
 from .glucose import CMR_glucose, calculate_SUV
 from .pve import PartialVolumeCorrection
+from .mrtrix3 import inclusion_filtering_mrtrix3
